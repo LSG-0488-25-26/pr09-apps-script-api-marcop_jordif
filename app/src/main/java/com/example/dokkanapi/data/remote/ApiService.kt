ba042c7a5d8e88163d8979eaae12159f89602627
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2026-03-12 22:43:54 +0100

    feat: ApiService

---
+++ app/src/main/java/com/example/dokkanapi/data/remote/ApiService.kt
@@ -0,0 +1,28 @@
+// data/remote/ApiService.kt
+package com.tuapp.dokkanbattle.data.remote
+
+import com.tuapp.dokkanbattle.data.model.ApiResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("exec")  // La teva URL base ja inclou /exec
+    suspend fun getAllCards(
+        @Query("api_key") apiKey: String,
+        @Query("action") action: String = "getAllCards"
+    ): ApiResponse<List<Map<String, Any>>>  // Retrofit convertirà automàticament
+
+    @GET("exec")
+    suspend fun getCardsByType(
+        @Query("api_key") apiKey: String,
+        @Query("action") action: String = "getCardsByType",
+        @Query("type") type: String
+    ): ApiResponse<List<Map<String, Any>>>
+
+    @GET("exec")
+    suspend fun getComments(
+        @Query("api_key") apiKey: String,
+        @Query("action") action: String = "getComments"
+    ): ApiResponse<List<Map<String, Any>>>
+}
