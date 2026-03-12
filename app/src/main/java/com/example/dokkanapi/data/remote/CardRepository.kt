c21b416f8bacec0e88fe1e99f2bfc7815195680c
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2026-03-12 22:47:16 +0100

    feat: CardRepositoy

---
+++ app/src/main/java/com/example/dokkanapi/data/remote/CardRepository.kt
@@ -0,0 +1,43 @@
+// data/repository/CardRepository.kt
+package com.tuapp.dokkanbattle.data.repository
+
+import com.tuapp.dokkanbattle.data.mapper.CardMapper
+import com.tuapp.dokkanbattle.data.model.Card
+import com.tuapp.dokkanbattle.data.remote.ApiService
+
+class CardRepository(
+    private val apiService: ApiService
+) {
+
+    suspend fun getAllCards(apiKey: String): Result<List<Card>> {
+        return try {
+            val response = apiService.getAllCards(apiKey)
+
+            if (response.success && response.data != null) {
+                // 👇 AQUÍ USES EL MAPPER
+                val cards = CardMapper.mapToCardList(response.data)
+                Result.success(cards)
+            } else {
+                Result.failure(Exception(response.error ?: "Error desconegut"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun getCardsByType(apiKey: String, type: String): Result<List<Card>> {
+        return try {
+            val response = apiService.getCardsByType(apiKey, type)
+
+            if (response.success && response.data != null) {
+                // 👇 AQUÍ TAMBÉ
+                val cards = CardMapper.mapToCardList(response.data)
+                Result.success(cards)
+            } else {
+                Result.failure(Exception(response.error ?: "Error desconegut"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+}
